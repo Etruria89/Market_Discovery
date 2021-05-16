6df5af953b067da7f3e3344b9999c622eb22772a
etruria89 <etruria89@hotmail.it>
2021-05-17 00:37:30 +0100

    Stock list filtering via csv added

--- prj_zero.py
+++ prj_zero.py
@@ -24,9 +24,9 @@ stocks = all_stocks['NASDAQ Symbol']
 data = {}
 table = {}
 tick = []
-_test = True
+_test = False
 _read = True
-_Revo = False
+_filter_list = False
 signal = 'RSI'
 _save = False
 buy_rsi_th = 30
@@ -34,7 +34,7 @@ sell_rsi_sell_th = 75
 sell_rsi_buy_th = 30
 
 # plot trigger
-_plot = True
+_plot = False
 
 # starting timer
 start_it = time.time()
@@ -45,13 +45,7 @@ info_arr = ["sector", "industry", "marketCap"]
 # Define tickers
 if _test:
 
-    if _Revo:
-        # Revo_DB
-        revo_db = pd.read_csv('Revolut_Stocks_List.csv', header=[0, 1])
-        revo_tick_lol = revo_db["Symbol"][:].values.tolist()
-        tick = [item for sublist in revo_tick_lol for item in sublist]
-    else:
-        tick = ['AAPL', 'AMZN']
+    tick = ['AAPL', 'AMZN']
 
 else:
 
@@ -96,93 +90,102 @@ data = {idx: gp.xs(idx, level=0, axis=1) for idx, gp in df.groupby(level=0, axis
 
 
 # Data processing
+if _filter_list:
+    # Revo_DB
+    revo_db = pd.read_csv('Revolut_Stocks_List.csv', header=[0, 1])
+    revo_tick_lol = revo_db["Symbol"][:].values.tolist()
+    tick = [item for sublist in revo_tick_lol for item in sublist]
+
 for stk in tick:
 
-    print('    Processing...', stk, all_stocks['Security Name'][stk], time.time()-start_it)
-    if len(data[stk].index) > window_long:
+    try:
+        print('    Processing...', stk, all_stocks['Security Name'][stk], time.time()-start_it)
+        if len(data[stk].index) > window_long:
 
-        # Price percentage change
-        data[stk]['diff_p'] = data[stk]['Close'].pct_change()
-        delta = data[stk]['diff_p']
-      
-        # Indicators
-            # MA
-        data[stk]['mean_short'] = data[stk]['Close'].rolling(window=window_short).mean()
-        data[stk]['mean_long'] = data[stk]['Close'].rolling(window=window_long).mean()
-            # RSI
-        up_days = delta.copy()
-        up_days[delta <= 0] = 0.0
-        down_days = abs(delta.copy())
-        down_days[delta > 0] = 0.0
-        RS_up = up_days.rolling(window_long).mean()
-        RS_down = down_days.rolling(window_long).mean()
-        data[stk]['rsi'] = 100.0 - 100.0 / (1.0 + RS_up / RS_down)
-        
-        # VOLATILITA'
-        #data[stk]['vol_7']= data[stk]['diff_p'].rolling(7).std() * np.sqrt(7) 
-        #data[stk]['vol_14']= data[stk]['diff_p'].rolling(14).std() * np.sqrt(14) 
-        #data[stk]['vol_21']= data[stk]['diff_p'].rolling(21).std() * np.sqrt(21) 
-        
-        #data[stk]['cumprod']=(1+data[stk]['diff_p']).cumprod()
-        
-        # Strategy
-        data[stk]['signal'] = 0.0
-        if signal == 'mean':
-            data[stk]['signal'][window_long:] = np.where(data[stk]['mean_short'][window_long:] > data[stk]['mean_long'][window_long:], 1.0, 0.0)
-            data[stk]['positions'] = data[stk]['signal'].diff()
-        elif signal == 'RSI':
-            data[stk]['signal_sell'] = (data[stk]['rsi'] > sell_rsi_sell_th) & (data[stk]['rsi'].shift(1) <= sell_rsi_sell_th)
-            data[stk]['signal_buy'] = (data[stk]['rsi'] < sell_rsi_buy_th) & (data[stk]['rsi'].shift(1) >= sell_rsi_buy_th)
-            data[stk]['positions'] = data[stk]['signal_sell'].astype(int) - data[stk]['signal_buy'].astype(int)
+            # Price percentage change
+            data[stk]['diff_p'] = data[stk]['Close'].pct_change()
+            delta = data[stk]['diff_p']
 
-        if  data[stk]['positions'][len(data[stk].index)-1] == 1:
-            table[stk] = all_stocks['Security Name'][stk]
-        #PLOT
-        if _plot:
-            fig = plt.figure() # Mean
-            # Add a subplot and label for y-axis
-            ax1 = fig.add_subplot(111,  ylabel='Price [$]')
-            # Plot the closing price
-            #data[stk]['Adj Close'].plot(ax=ax1, color='r', lw=2.)
-            # Plot the short and long moving averages
-            data[stk][['mean_short', 'mean_long']].plot(ax=ax1, lw=2.)
-            # Add the title
-            plt.title(stk + " MAs")
+            # Indicators
+                # MA
+            data[stk]['mean_short'] = data[stk]['Close'].rolling(window=window_short).mean()
+            data[stk]['mean_long'] = data[stk]['Close'].rolling(window=window_long).mean()
+                # RSI
+            up_days = delta.copy()
+            up_days[delta <= 0] = 0.0
+            down_days = abs(delta.copy())
+            down_days[delta > 0] = 0.0
+            RS_up = up_days.rolling(window_long).mean()
+            RS_down = down_days.rolling(window_long).mean()
+            data[stk]['rsi'] = 100.0 - 100.0 / (1.0 + RS_up / RS_down)
+
+            # VOLATILITA'
+            #data[stk]['vol_7']= data[stk]['diff_p'].rolling(7).std() * np.sqrt(7)
+            #data[stk]['vol_14']= data[stk]['diff_p'].rolling(14).std() * np.sqrt(14)
+            #data[stk]['vol_21']= data[stk]['diff_p'].rolling(21).std() * np.sqrt(21)
+
+            #data[stk]['cumprod']=(1+data[stk]['diff_p']).cumprod()
+
+            # Strategy
+            data[stk]['signal'] = 0.0
             if signal == 'mean':
-                # Plot the buy signals
-                ax1.plot(data[stk].loc[data[stk]['positions'] == 1.0].index,
-                         data[stk]['mean_short'][data[stk]['positions'] == 1.0],
-                         '^', markersize=10, color='m')
-                # Plot the sell signals
-                ax1.plot(data[stk].loc[data[stk]['positions'] == -1.0].index,
-                         data[stk]['mean_short'][data[stk]['positions'] == -1.0],
-                         'v', markersize=10, color='k')
+                data[stk]['signal'][window_long:] = np.where(data[stk]['mean_short'][window_long:] > data[stk]['mean_long'][window_long:], 1.0, 0.0)
+                data[stk]['positions'] = data[stk]['signal'].diff()
+            elif signal == 'RSI':
+                data[stk]['signal_sell'] = (data[stk]['rsi'] > sell_rsi_sell_th) & (data[stk]['rsi'].shift(1) <= sell_rsi_sell_th)
+                data[stk]['signal_buy'] = (data[stk]['rsi'] < sell_rsi_buy_th) & (data[stk]['rsi'].shift(1) >= sell_rsi_buy_th)
+                data[stk]['positions'] = data[stk]['signal_sell'].astype(int) - data[stk]['signal_buy'].astype(int)
 
-            # Plot RSI
-            fig_rsi = plt.figure("RSI") # RSI
-            ax_rsi = fig_rsi.add_subplot(111, ylabel='RSI [%]')
-            data[stk][['rsi']].plot(ax=ax_rsi)
-            # Add the title
-            plt.title(stk + " RSI plot")
-            if signal == 'RSI':
-                # Plot the buy signals
-                ax_rsi.plot(data[stk].loc[data[stk]['positions'] == 1.0].index,
-                         data[stk]['rsi'][data[stk]['positions'] == 1.0],
-                         '^', markersize=10, color='m')
-                # Plot the sell signals
-                ax_rsi.plot(data[stk].loc[data[stk]['positions'] == -1.0].index,
-                         data[stk]['rsi'][data[stk]['positions'] == -1.0],
-                         'v', markersize=10, color='k')
+            if  data[stk]['positions'][len(data[stk].index)-1] == 1:
+                table[stk] = all_stocks['Security Name'][stk]
+            #PLOT
+            if _plot:
+                fig = plt.figure() # Mean
+                # Add a subplot and label for y-axis
+                ax1 = fig.add_subplot(111,  ylabel='Price [$]')
+                # Plot the closing price
+                #data[stk]['Adj Close'].plot(ax=ax1, color='r', lw=2.)
+                # Plot the short and long moving averages
+                data[stk][['mean_short', 'mean_long']].plot(ax=ax1, lw=2.)
+                # Add the title
+                plt.title(stk + " MAs")
+                if signal == 'mean':
+                    # Plot the buy signals
+                    ax1.plot(data[stk].loc[data[stk]['positions'] == 1.0].index,
+                             data[stk]['mean_short'][data[stk]['positions'] == 1.0],
+                             '^', markersize=10, color='m')
+                    # Plot the sell signals
+                    ax1.plot(data[stk].loc[data[stk]['positions'] == -1.0].index,
+                             data[stk]['mean_short'][data[stk]['positions'] == -1.0],
+                             'v', markersize=10, color='k')
 
-            # Show the plot for the first stock
-            plt.show()
+                # Plot RSI
+                fig_rsi = plt.figure("RSI") # RSI
+                ax_rsi = fig_rsi.add_subplot(111, ylabel='RSI [%]')
+                data[stk][['rsi']].plot(ax=ax_rsi)
+                # Add the title
+                plt.title(stk + " RSI plot")
+                if signal == 'RSI':
+                    # Plot the buy signals
+                    ax_rsi.plot(data[stk].loc[data[stk]['positions'] == 1.0].index,
+                             data[stk]['rsi'][data[stk]['positions'] == 1.0],
+                             '^', markersize=10, color='m')
+                    # Plot the sell signals
+                    ax_rsi.plot(data[stk].loc[data[stk]['positions'] == -1.0].index,
+                             data[stk]['rsi'][data[stk]['positions'] == -1.0],
+                             'v', markersize=10, color='k')
 
-        #EARNINGS
-        data[stk]['portfolio'] = 1 * data[stk]['signal'].shift(1)
-        #data[stk]['portfolio_value'] = data[stk]['portfolio'].multiply(data[stk]['Adj Close'])
-        data[stk]['portfolio_return'] = data[stk]['portfolio'].multiply(data[stk]['diff_p']).cumsum()
-    else:
-        data.pop(stk)
+                # Show the plot for the first stock
+                plt.show()
+
+            #EARNINGS
+            data[stk]['portfolio'] = 1 * data[stk]['signal'].shift(1)
+            #data[stk]['portfolio_value'] = data[stk]['portfolio'].multiply(data[stk]['Adj Close'])
+            data[stk]['portfolio_return'] = data[stk]['portfolio'].multiply(data[stk]['diff_p']).cumsum()
+        else:
+            data.pop(stk)
+    except:
+        print(stk + " not available")
 
 print('end Processing', time.time()-start_it)
 print(table)
